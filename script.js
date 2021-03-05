f4affa3673fca075dce8ee5a75e4ac18f2d6e362
Leonardo200213 <50982851+Leonardo200213@users.noreply.github.com>
2021-03-05 18:07:29 +0100

    modificato js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("b1").innerHTML = this.responseText;
-        }
+function javascript() {
+	var dati = document.getElementById("calcolo").value;
+    var xhr = new XMLHttpRequest(); //XMLHttpRequest è usato per richiedere dati da un server web
+    xhr.onreadystatechange = function() { //onreadystatechange definisce una funzione che deve essere eseguita quando readyState cambia. readyState mantiene lo stato di XMLHttpRequest
+        if (this.readyState == 4 && this.status == 200) { //readyState = 4 significa che la richiesta è andata a buon fine e che la risposta è pronta
+            document.getElementById("risultato").innerHTML = "Risultato: " + this.responseText; //responseText ritorna come stringa il corpo del server
+        }else{
+			document.getElementById("risultato").innerHTML = "Errore";
+		}
     };
-    xhttp.open("GET", "http://api.mathjs.org/v4/?expr=2*(7-3)", true);
-    xhttp.send();
+    xhr.open("GET", "http://api.mathjs.org/v4/?expr=" + dati, false);
+    xhr.send();
 }
 
 
 function jquery() {
     $(document).ready(function() {
         $.ajax({
-            url: "http://api.mathjs.org/v4/?expr=2*(7-3)",
+            url: "http://api.mathjs.org/v4/",
             success: function(result) {
                 $("#b2").html(result);
             }
