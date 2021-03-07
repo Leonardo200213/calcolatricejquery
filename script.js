177442470bcbbdb4838a541cab4028378db91247
Leonardo200213 <50982851+Leonardo200213@users.noreply.github.com>
2021-03-07 16:08:07 +0100

    modificato jquery

--- script.js
+++ script.js
@@ -38,38 +38,34 @@ function jquery() {
 
 
 function javascriptpost() {
-	var dati = document.getElementById("calcolo").value;
-    var xhr = new XMLHttpRequest(); //XMLHttpRequest è usato per richiedere dati da un server web
-    xhr.onreadystatechange = function() { //onreadystatechange definisce una funzione che deve essere eseguita quando readyState cambia. readyState mantiene lo stato di XMLHttpRequest
-        if (this.readyState == 4 && this.status == 200) { //readyState = 4 significa che la richiesta è andata a buon fine e che la risposta è pronta
-            document.getElementById("risultato").innerHTML = "Risultato: " + this.responseText; //responseText ritorna come stringa il corpo del server
-        }else{
-			document.getElementById("risultato").innerHTML = "Errore";
+	var input = document.getElementById("postcalcolo").value;
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("postrisultato").innerHTML = this.responseText;
 		}
-    };
-    xhr.open("GET", "http://api.mathjs.org/v4/?expr=" + dati, false);
-    xhr.send();
+	};
+	xhttp.open("POST", "http://api.mathjs.org/v4/", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send({
+		"expr": [input],
+		"precision": 14
+	});
 }
 
 function jquerypost() {
-	$( document ).ready(function() {
-	  var formula = $('#postcalcoloq').val();
-		var postData = {
-		"expr": [ formula ],
-		"precision": 14
-	  };
+    var input = $('#postcalcoloq').val();
+    var dati = {
+        "expr": [input],
+        "precision": 14
+    };
 
-		$.ajax({
-		url: 'https://api.mathjs.org/v4/',
-		type: 'post',
-		contentType: 'application/json',
-		data: JSON.stringify(postData),
-		success: function( data, textStatus, jQxhr ){
-		  $("#postrisultatoq").html(data.result[0]);
-		},
-		error: function() {
-				$("#risultatoq").html("Errore");
-			}
-	  });
-	});
+    $.ajax({
+        url: 'https://api.mathjs.org/v4/',
+        type: 'POST',
+        contenttype: 'application/json',
+        data: JSON.stringify(dati),
+        success: function(data, textStatus, jQxhr) { $("#risultatojq").html(data.result[0]); }
+    });
 }
